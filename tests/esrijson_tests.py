e5b865138d318d6184a276f25235bf7c6eaf1046
Ian Dees <ian.dees@gmail.com>
2016-08-26 00:53:03 -0400

    Add tests based on https://github.com/Esri/arcgis-to-geojson-utils/blob/master/test/index.js

--- tests/esrijson_tests.py
+++ tests/esrijson_tests.py
@@ -38,6 +38,47 @@ class TestEsriJsonToGeoJson(unittest.TestCase):
         }
         self.assertDictEqual(out_json, expected_geojson)
 
+    def test_multi_point(self):
+        d = EsriDumper('http://example.com')
+
+        in_json = {
+            "geometry": {
+                "points": [
+                    [41.83, 71.01],
+                    [56.95, 33.75],
+                    [21.79, 36.56]
+                ],
+            },
+            "attributes": {
+                "string_prop": 151,
+                "float_prop": 1004.623,
+                "str_prop": "County Road",
+                "str_int_prop": "16",
+            }
+        }
+        out_json = d._build_geojson('esriGeometryPoint', in_json)
+
+        expected_geojson = {
+            "type": "Feature",
+            "properties": {
+                "string_prop": 151,
+                "float_prop": 1004.623,
+                "str_prop": "County Road",
+                "str_int_prop": "16",
+            },
+            "geometry": {
+                "type": "MultiPoint",
+                "coordinates": [
+                    [
+                        [41.83, 71.01],
+                        [56.95, 33.75],
+                        [21.79, 36.56]
+                    ]
+                ]
+            }
+        }
+        self.assertDictEqual(out_json, expected_geojson)
+
     def test_empty_point(self):
         d = EsriDumper('http://example.com')
 
@@ -58,7 +99,30 @@ class TestEsriJsonToGeoJson(unittest.TestCase):
         }
         self.assertDictEqual(out_json, expected_geojson)
 
-    def test_polyline(self):
+    def test_linestring(self):
+        d = EsriDumper('http://example.com')
+
+        in_json = {
+            "geometry": {
+                "paths" : [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832]]
+                ],
+            }
+        }
+        out_json = d._build_geojson('esriGeometryPolyline', in_json)
+
+        expected_geojson = {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832]
+                ],
+            }
+        }
+        self.assertDictEqual(out_json, expected_geojson)
+
+    def test_multi_linestring(self):
         d = EsriDumper('http://example.com')
 
         in_json = {
@@ -74,7 +138,7 @@ class TestEsriJsonToGeoJson(unittest.TestCase):
         expected_geojson = {
             "type": "Feature",
             "geometry": {
-                "type": "LineString",
+                "type": "MultiLineString",
                 "coordinates": [
                     [ [-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832] ],
                     [ [-97.06326,32.759], [-97.06298,32.755] ]
@@ -84,6 +148,78 @@ class TestEsriJsonToGeoJson(unittest.TestCase):
         self.assertDictEqual(out_json, expected_geojson)
 
     def test_polygon(self):
+        d = EsriDumper('http://example.com')
+
+        in_json = {
+            "geometry": {
+                "rings" : [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]]
+                ],
+            }
+        }
+        out_json = d._build_geojson('esriGeometryPolygon', in_json)
+
+        expected_geojson = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]]
+                ],
+            }
+        }
+        self.assertDictEqual(out_json, expected_geojson)
+
+    def test_polygon_close(self):
+        # Esri-JSON allows polygons that aren't closed. GeoJSON requires them to be closed.
+        d = EsriDumper('http://example.com')
+
+        in_json = {
+            "geometry": {
+                "rings" : [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832]]
+                ],
+            }
+        }
+        out_json = d._build_geojson('esriGeometryPolygon', in_json)
+
+        expected_geojson = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]]
+                ],
+            }
+        }
+        self.assertDictEqual(out_json, expected_geojson)
+
+    def test_polygon_strip_invalid_rings(self):
+        # Esri JSON allows rings with three points (A-B-A) that are essentially lines. GeoJSON doesn't.
+        d = EsriDumper('http://example.com')
+
+        in_json = {
+            "geometry": {
+                "rings" : [
+                    [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]],
+                    [[-97.06326,32.759], [-97.06298,32.755], [-97.06326,32.759]]
+                ],
+            }
+        }
+        out_json = d._build_geojson('esriGeometryPolygon', in_json)
+
+        expected_geojson = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]
+                ],
+            }
+        }
+        self.assertDictEqual(out_json, expected_geojson)
+
+    def test_multi_polygon(self):
         d = EsriDumper('http://example.com')
 
         in_json = {
@@ -99,7 +235,7 @@ class TestEsriJsonToGeoJson(unittest.TestCase):
         expected_geojson = {
             "type": "Feature",
             "geometry": {
-                "type": "Polygon",
+                "type": "MultiPolygon",
                 "coordinates": [
                     [[-97.06138,32.837], [-97.06133,32.836], [-97.06124,32.834], [-97.06127,32.832], [-97.06138,32.837]],
                     [[-97.06326,32.759], [-97.06298,32.755], [-97.06153,32.749], [-97.06326,32.759]]
