766310bacd086d935f478bfe31e7a8d588642a70
Ian Dees <ian.dees@gmail.com>
2016-08-26 19:54:00 -0400

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='esridump',
-    version='1.0.0',
+    version='1.1.0',
     description='Dump geodata from ESRI endpoints to GeoJSON',
     long_description=readme,
     author='Ian Dees',
