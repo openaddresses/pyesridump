0c50fc402f4234a516266c23029bbbec2029fff8
Ian Dees <ian.dees@gmail.com>
2017-05-28 11:08:14 -0400

    Fix test

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -104,6 +104,6 @@ class TestEsriDumpCommandlineMain(unittest.TestCase):
 
         esridump.cli.main()
 
-        self.assertIn('where=foo%3Dbar&', self.responses.calls[2].request.url)
+        self.assertIn('where=foo%3Dbar', self.responses.calls[2].request.url)
         self.assertIn('where=%28OBJECTID+%3E%3D+70193+AND+OBJECTID+%3C%3D+70307%29+AND+%28foo%3Dbar%29', self.responses.calls[3].request.body)
         self.assertEqual(self.mock_outfile.write.call_count, 14)
