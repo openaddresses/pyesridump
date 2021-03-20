1793d61f2c50bd46c333290b4c280cedc5824dfb
Ian Dees <ian.dees@gmail.com>
2021-03-20 11:19:17 -0500

    Bump version: 1.10.0 → 1.10.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='esridump',
-    version='1.10.0',
+    version='1.10.1',
     description='Dump geodata from ESRI endpoints to GeoJSON',
     long_description=readme,
     long_description_content_type='text/markdown',
